ac9bc2367b9f693499930c203b01b20fde09b703
Sharlyn <153042415+ssSharlyn@users.noreply.github.com>
2026-03-20 14:33:25 +0800

    Delete 调度器模板 directory

--- 调度器模板/src/driver/ds1302.h
+++
@@ -1,5 +0,0 @@
-#include <STC15F2K60S2.H>
-#include <intrins.h>
-
-void write_rtc(unsigned char *ucRTC);
-void read_rtc(unsigned char *ucRTC);
--- 调度器模板/src/driver/ultra.h
+++
@@ -1,4 +0,0 @@
-#include <STC15F2K60S2.H>
-#include <intrins.h>
-
-unsigned int ultra_dat(void);
