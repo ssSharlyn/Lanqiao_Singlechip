3f68f2d1f3f885b691fc99230c08554d82188527
Sharlyn <153042415+ssSharlyn@users.noreply.github.com>
2026-03-21 21:36:46 +0800

    Delete 调度器模板 directory

--- 调度器模板/src/driver/iic.h
+++
@@ -1,7 +0,0 @@
-#include <STC15F2K60S2.H>
-#include <intrins.h>
-
-void da_write(unsigned char dat);
-unsigned char ad_read(unsigned char addr);
-void eeprom_write(unsigned char *str, unsigned char addr, unsigned char num);
-void eeprom_read(unsigned char *str, unsigned char addr, unsigned char num);
--- 调度器模板/src/driver/uart.h
+++
@@ -1,4 +0,0 @@
-#include <STC15F2K60S2.H>
-#include <stdio.h>
-
-void uart1_init(void);
